9257d07e061ff3b3ec05c8e28c6d08dacfc1b9d8
Alfredo Rodriguez <arodrigu12@hotmail.com>
2019-10-23 18:00:44 -0400

    Add project files.

---
+++ SimpleCSharpApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleCSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display a simple message to the user
+            Console.WriteLine("***** My First C# App *****");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+            // Process any incoming args
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Arg: {0}", args[i]);
+            }
+
+            // Wait for enter key to be pressed before shutting down
+            Console.ReadLine();
+        }
+    }
+}
